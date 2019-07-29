c27d8954862a1d3506b5e101c79db55492aaeaca
jimmyknits42 <jimmynoehren@gmail.com>
2019-07-29 14:35:22 -0700

    Create booleanOps.js

---
+++ booleanOps.js
@@ -0,0 +1,104 @@
+/**
+ * passesOR
+ * Returns true if at least one element in the list is true
+ *
+ * @param {Array} bools - Array of true/false values
+ *
+ * @example
+ * passesOR([true, true, true]); //returns true
+ * @example
+ * passesOR([true, false, true]); //returns true
+ * @example
+ * passesOR([false, false, false]); //returns false
+ */
+function passesOR(bools) {
+  var ret = false;
+  if (bools.length > 0) {
+    for (var i = 0; i < bools.length; i++) {
+      if (bools[i] == true) {
+        ret = true;
+        break;
+      }
+    }
+  }
+  return ret;
+}
+
+/**
+ * passesAND
+ * Returns true if all elements in the list are true
+ *
+ * @param {Array} bools - Array of true/values falues
+ *
+ * @example
+ * passesAND([true, true, true]); //returns true
+ * @example
+ * passesAND([true, false, true]); //returns false
+ * @example
+ * passesAND([false, false, false]); //returns false
+ */
+function passesAND(bools) {
+  var ret = true;
+  if (bools.length > 0) {
+    for (var i = 0; i < bools.length; i++) {
+      if (bools[i] == false) {
+        ret = false;
+        break;
+      }
+    }
+  }
+  else {
+    ret = false;
+  }
+  return ret;
+}
+
+/**
+ * passesBoolCondition
+ * Returns true if the list of booleans passes AND or OR logic
+ * If operator is not supplied, AND logic is used
+ *
+ * @param {Array} bools - list of true/false values
+ * @param {string} operator - operation of choice ("And" or "Or")
+ *
+ * @example
+ * passesBoolCondition([]); //returns false
+ * @example
+ * passesBoolCondition([true]); //returns true
+ * @example
+ * passesBoolCondition([true, true]); //returns true
+ * @example
+ * passesBoolCondition([true, false]); //returns false
+ * @example
+ * passesBoolCondition([true, true, true], "And"); //returns true
+ * @example
+ * passesBoolCondition([true, false, true], "And"); //returns false
+ * @example
+ * passesBoolCondition([true, true, true], "Or"); //returns true
+ * @example
+ * passesBoolCondition([true, false, true], "Or"); //returns true
+ */
+function passesBoolCondition(bools, operator) {
+  if (bools.length == 0) {
+    return false;
+  }
+  else if (bools.length == 1) {
+    return bools[0];
+  }
+  else if (bools.length > 1) {
+    if (operator == null) {
+      return passesAND(bools);
+    }
+    else {
+      switch (operator) {
+        case "And":
+          return passesAND(bools);
+        case "Or":
+          return passesOR(bools);
+        default: 
+          message("somehow have " + bools.length + " booleans, but the operator is " + operator);
+          return false;
+      }
+    }
+  }
+}
