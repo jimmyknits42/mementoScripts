17223247208f0f7ab72dc0d0914c40c7a8ed3ef5
jimmyknits42 <jimmynoehren@gmail.com>
2020-02-03 15:51:44 -0800

    return entry for start

--- dates.js
+++ dates.js
@@ -40,7 +40,7 @@ function getDurationString(entry) {
     var aoDate = entry.field("As Of Date");
     var aoDateUnc = entry.field("As Of Date Uncertainty");
     if (aoDate != null && aoDate != undefined) { 
-      start = aoDate/*moment(aoDate).format("YYYY-MM-DD") + getNumericUncertaintyString(aoDateUnc)*/;
+      start = entry/*moment(aoDate).format("YYYY-MM-DD") + getNumericUncertaintyString(aoDateUnc)*/;
     }
     else {
       var aoYear = entry.field("As Of Year");
