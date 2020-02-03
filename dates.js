76e76ef8c154c253d71aa975bf7a14e687800d95
jimmyknits42 <jimmynoehren@gmail.com>
2020-02-03 15:54:11 -0800

    changed name of parameter from entry to e

--- dates.js
+++ dates.js
@@ -24,31 +24,31 @@
  * If no start moment is specified, it will be "???"
  * If no end moment is specified, it will be "Now"
  *
- * @param {object} entry - entry to calculate duration string for
+ * @param {object} e - entry to calculate duration string for
  *
  * @returns {string}
  */
-function getDurationString(entry) {
+function getDurationString(e) {
   var ret = "";
 
-  if (false/*entry.field("All Time")*/) {
+  if (false/*e.field("All Time")*/) {
     ret = "(All Time)";
   }
   else {
     //construct start date
     var start = "";
-    var aoDate = entry.field("As Of Date");
-    var aoDateUnc = entry.field("As Of Date Uncertainty");
+    var aoDate = e.field("As Of Date");
+    var aoDateUnc = e.field("As Of Date Uncertainty");
     if (aoDate != null && aoDate != undefined) { 
-      start = entry/*moment(aoDate).format("YYYY-MM-DD") + getNumericUncertaintyString(aoDateUnc)*/;
+      start = e/*moment(aoDate).format("YYYY-MM-DD") + getNumericUncertaintyString(aoDateUnc)*/;
     }
     else {
-      var aoYear = entry.field("As Of Year");
-      var aoYearUnc = entry.field("As Of Year Uncertainty");
-      var aoMonth = entry.field("As Of Month");
-      var aoMonthUnc = entry.field("As Of Month Uncertainty");
-      var aoDay = entry.field("As Of Day");
-      var aoDayUnc = entry.field("As Of Day Uncertainty");
+      var aoYear = e.field("As Of Year");
+      var aoYearUnc = e.field("As Of Year Uncertainty");
+      var aoMonth = e.field("As Of Month");
+      var aoMonthUnc = e.field("As Of Month Uncertainty");
+      var aoDay = e.field("As Of Day");
+      var aoDayUnc = e.field("As Of Day Uncertainty");
       if (aoYear > 0) {
         start = aoYear + getNumericUncertaintyString(aoYearUnc);
       }
@@ -70,18 +70,18 @@ function getDurationString(entry) {
     }
     //construct end date
     var end = "";
-    var uDate = entry.field("Until Date");
-    var uDateUnc = entry.field("Until Date Uncertainty");
+    var uDate = e.field("Until Date");
+    var uDateUnc = e.field("Until Date Uncertainty");
     if (uDate != null && uDate != undefined) {
       end = moment(uDate).format("YYYY-MM-DD") + getNumericUncertaintyString(uDateUnc);
     }
     else {
-      var uYear = entry.field("Until Year");
-      var uYearUnc = entry.field("Until Year Uncertainty");
-      var uMonth = entry.field("Until Month");
-      var uMonthUnc = entry.field("Until Month Uncertainty");
-      var uDay = entry.field("Until Day");
-      var uDayUnc = entry.field("Until Day Uncertainty");
+      var uYear = e.field("Until Year");
+      var uYearUnc = e.field("Until Year Uncertainty");
+      var uMonth = e.field("Until Month");
+      var uMonthUnc = e.field("Until Month Uncertainty");
+      var uDay = e.field("Until Day");
+      var uDayUnc = e.field("Until Day Uncertainty");
       if (uYear > 0) {
         end = uYear + getNumericUncertaintyString(uYearUnc);
       }
