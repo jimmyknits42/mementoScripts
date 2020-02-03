06131ebffe360eafe5902a274d2e66ea8eed59de
jimmyknits42 <jimmynoehren@gmail.com>
2020-02-03 11:28:32 -0800

    created dates.js

---
+++ dates.js
@@ -0,0 +1,107 @@
+/** 
+ * getDurationString
+ * Returns the string that represents duration
+ * Expects the context entry to contain the following fields
+ *   "All Time"
+ *   "As Of Timeline Type (Reference)"
+ *   "As Of Date"
+ *   "As Of Date Uncertainty"
+ *   "As Of Year"
+ *   "As Of Year Uncertainty"
+ *   "As Of Month" 
+ *   "As Of Month Uncertainty"
+ *   "As Of Day"
+ *   "As Of Day Uncertainty"
+ *   "Until Timeline Type (Reference)"
+ *   "Until Date"
+ *   "Until Date Uncertainty"
+ *   "Until Year"
+ *   "Until Year Uncertainty"
+ *   "Until Month"
+ *   "Until Month Uncertainty"
+ *   "Until Day"
+ *   "Until Day Uncertainty"
+ * If no start moment is specified, it will be "???"
+ * If no end moment is specified, it will be "Now"
+ *
+ * @returns {string}
+ */
+function getDurationString() {
+  var ret = "";
+
+  if (field("All Time")) {
+    ret = "(All Time)";
+  }
+  else {
+    //construct start date
+    var start = "";
+    var aoDate = field("As Of Date");
+    var aoDateUnc = field("As Of Date Uncertainty");
+    if (aoDate != null) { 
+      start = moment(aoDate).format("YYYY-MM-DD") + getNumericUncertaintyString(aoDateUnc);
+    }
+    else {
+      var aoYear = field("As Of Year");
+      var aoYearUnc = field("As Of Year Uncertainty");
+      var aoMonth = field("As Of Month");
+      var aoMonthUnc = field("As Of Month Uncertainty");
+      var aoDay = field("As Of Day");
+      var aoDayUnc = field("As Of Day Uncertainty");
+      if (aoYear > 0) {
+        start = aoYear + getNumericUncertaintyString(aoYearUnc);
+      }
+      if (aoMonth > 0) {
+        if (start != "") {
+          start += "-";
+        }
+        start += aoMonth + getNumericUncertaintyString(aoMonthUnc);
+      }
+      if (aoDay > 0) {
+        if (start != "") {
+          start += "-"
+        }
+        start += aoDay + getNumericUncertaintyString(aoDayUnc);
+      }
+    }
+    if (start == "") {
+      start = "???";
+    }
+    //construct end date
+    var end = "";
+    var uDate = field("Until Date");
+    var uDateUnc = field("Until Date Uncertainty");
+    if (uDate != null) {
+      end = moment(uDate).format("YYYY-MM-DD") + getNumericUncertaintyString(uDateUnc);
+    }
+    else {
+      var uYear = field("Until Year");
+      var uYearUnc = field("Until Year Uncertainty");
+      var uMonth = field("Until Month");
+      var uMonthUnc = field("Until Month Uncertainty");
+      var uDay = field("Until Day");
+      var uDayUnc = field("Until Day Uncertainty");
+      if (uYear > 0) {
+        end = uYear + getNumericUncertaintyString(uYearUnc);
+      }
+      if (uMonth > 0) {
+        if (end != "") {
+          end += "-";
+        }
+        end += uMonth + getNumericUncertaintyString(uMonthUnc);
+      }
+      if (uDay > 0) {
+        if (end != "") {
+          end += "-"
+        }
+        end += uDay + getNumericUncertaintyString(uDayUnc);
+      }
+    }
+    if (end == "") {
+      end = "Now";
+    }
+    //construct duration string
+    ret = "(" + start + " - " + end + ")";
+  }
+
+  return ret
+}
