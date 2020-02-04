e7ac7286ebe835413b5582b89d237b7ebb78d1f4
jimmyknits42 <jimmynoehren@gmail.com>
2020-02-03 16:11:04 -0800

    added getDateString
    getDurationString doesn't work because I have to call field functions on the entry which doesn't work in all contexts, but if I parameterize the function, it should work

--- dates.js
+++ dates.js
@@ -107,3 +107,56 @@ function getDurationString(e) {
 
   return ret
 }
+
+
+/**
+ * getDateString
+ * Returns a constructed date string with the format:
+ *   YYYY-MM-DD
+ * if any value has numeric uncertainty, then it is included in the result
+ *
+ * @param {date} date - specific date to be formatted and returned
+ * @param {Array} dateUnc - array of string values corresponding to uncertainty of date value
+ * @param {number} year - year to be formatted and returned
+ * @param {Array} yearUnc - array of string values corresponding to uncertainty of year value
+ * @param {number} month - month to be formatted and return, 1-12, will be left-padded with 0s
+ * @param {Array} monthUnc - array of string values corresponding to uncertainty of month value
+ * @param {number} day - day to be formatted and returned, 1-31, will be left-padded with 0s
+ * @param {Array} dayUnc - array of string values corresponding to uncertainty of day value
+ * @param {string} def - default value to be returned if all other parameters are empty
+ *
+ * @returns {string}
+ */
+function getDateString(date, dateUnc, year, yearUnc, month, monthUnc, day, dayUnc, def) {
+  var ret = "";
+
+  //construct string, prefer date if available otherwise use other values
+  if (date != null && date != undefined) {
+    ret = moment(date).format("YYYY-MM-DD") + getNumericUncertaintyString(dateUnc);
+  }
+  else {
+    if (year > 0) {
+      ret = year + getNumericUncertaintyString(yearUnc);
+    }
+    if (month > 0) {
+      if (ret != "") {
+        ret += "-";
+      }
+      ret += pad("00", month.toString(), true) + getNumericUncertaintyString(monthUnc);
+    }
+    if (day > 0) {
+      if (ret != "") {
+        ret += "-"
+      }
+      ret += pad("00", day.toString(), true) + getNumericUncertaintyString(dayUnc);
+    }
+  }
+
+  //if the date string is still empty, return the default value
+  if (ret == "") {
+    ret = def;
+  }
+
+  //return
+  return ret
+}
