523c42850fb14d75593c9ae74d77b0835ec9c4c1
jimmyknits42 <jimmynoehren@gmail.com>
2020-02-13 15:06:40 -0800

    added getFieldValue and getDisplayID
    easier ways to return the displayIDs and, generically, the value of any field from a set of entries. useful for "Link to Entry" field types where you want to display the displayID

--- strings.js
+++ strings.js
@@ -17,3 +17,50 @@
     return (str + pad).substring(0, pad.length);
   }
 }
+
+
+
+/**
+ * getFieldValue
+ * Returns the value of a specified field from the entry/entries in the e parameter
+ * If there is more than one entry in e, the values are comma separated
+ *
+ * @param {Array} e - entry/entries to return field value for
+ * @param {string} fieldName - name of the field whose value will be returned for each entry
+ * @param {string} delimiter - value to separate the fieldName values if there are multiple entries
+ *
+ * @returns {string}
+ */
+function getFieldValue(e, fieldName, delimiter) {
+  //initialize
+  var ret = "";
+  //exit out if invalid input
+  if (e == null || typeof e === 'undefined' || e.length == 0) {
+    return ret
+  }
+  //if there is at least one entry in the parameter, return it's field value
+  if (e.length > 0) {
+    ret = e[0].field(fieldName);
+  }
+  //if there is more than one entry, comma separate the others
+  if (e.length > 1) {
+    for (var i = 1; i < e.length; i++) {
+      ret += delimiter + e[i].field(fieldName);
+    }
+  }
+  //return
+  return ret
+}
+
+/**
+ * getDisplayID
+ * Returns a string of the displayID(s) of the entry/entries in the e parameter
+ * If there is more than one entry in the parameter, they are comma separated
+ * 
+ * @param {Array} e - entry/entries to return displayID for
+ *
+ * @returns {string}
+ */
+function getDisplayID(e) {
+  return getFieldValue(e, "displayID", ", ")
+}
