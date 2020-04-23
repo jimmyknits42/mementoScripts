838fc8956420b562bd0ef8e31216b539ffd589cb
jimmyknits42 <jimmynoehren@gmail.com>
2020-04-23 00:11:02 -0700

    added string + uncertainty string functions
    getStringWithUncertaintyString
    getLeftPaddedNumberString
    getLeftPaddedNumberStringWithUncertaintyString

--- uncertaintyStrings.js
+++ uncertaintyStrings.js
@@ -137,3 +137,91 @@ function getSpacerString(prefix, suffix, spacer) {
 function getSlashString(prefix, suffix) {
   return getSpacerString(prefix, suffix, "/");
 }
+
+/**
+ * getStringWithUncertaintyString
+ * Returns the str parameter with the numeric uncertainty string appended based on the uncertainty parameter
+ * if the str parameter is empty, empty string is returned
+ * 
+ * @param {string} str - string to append uncertainty string to
+ * @param {Array} uncertainty - array of string values corresponding to the numeric uncertainty of str
+ *
+ * @returns {string}
+ */
+function getStringWithUncertaintyString(str, uncertainty) {
+  //initialize return
+  var ret = "";
+
+  //if str is not empty
+  if (str != "") {
+    //set return value to str + uncertainty string
+    ret = string + getNumericUncertaintyString(uncertainty);
+  }
+
+  //return
+  return ret
+}
+
+/**
+ * getLeftPaddedNumberString
+ * Returns the number parameter left-padded with p parameter
+ * 
+ * @param {number} num - number to left-pad
+ * @param {string} padding - padding to use, of the desired length (empty string indicates no padding)
+ * @param {boolean} allowZero - true = a string will be returned even if num is 0; false = if num is 0, empty string will be returned
+ * 
+ * @example
+ * // returns "4"
+ * getLeftPaddedNumberString(4, "", false)
+ * @example
+ * // returns "4"
+ * getLeftPaddedNumberString(4, "0", false)
+ * @example
+ * // returns "04"
+ * getLeftPaddedNumberString(4, "00", false)
+ * @example
+ * // returns "-04"
+ * getLeftPaddedNumberString(-4, "00", false)
+ * @example
+ * // returns ""
+ * getLeftPaddedNumberString(0, "00", false)
+ * @example
+ * // returns "00"
+ * getLeftPaddedNumberString(0, "00", true)
+ *
+ * @returns {string}
+ */
+function getLeftPaddedNumberString(num, padding, allowZero) {
+  //initialize return
+  var ret = "";
+
+  //if it doesn't matter if the number value is 0 OR if it can't be 0 and it isn't 0
+  if (allowZero || (!allowZero && Math.abs(num) != 0)) {
+      //set the return value to left-pad the absolute value of the number parameter
+      ret = pad(padding, Math.abs(num).toString(), true);
+      //if the number parameter is negative
+      if (num < 0) {
+        //prefix the return value with a minus sign
+        ret = "-" + ret;
+      }
+  }
+
+
+  //return
+  return ret
+}
+
+/**
+ * getLeftPaddedNumberWithUncertaintyString
+ * Returns a string with the num parameter left-padded with p and any uncertainty appended
+ *
+ * @param {number} num - number to pad and has uncertainty
+ * @param {string} p - the padding to use, of the desired length (empty string indicates no padding)
+ * @param {boolean} allowZero - true = a string will be returned even if num is 0; false = if num is 0, empty string will be returned
+ * @param {Array} uncertainty - array of strings corresponding to the uncertainty of the number parameter
+ *
+ * @returs {string}
+ */
+function getLeftPaddedNumberWithUncertaintyString(num, padding, allowZero, uncertainty) {
+  return getStringWithUncertaintyString(getLeftPaddedNumberString(num, padding, allowZero), uncertainty)
+}
